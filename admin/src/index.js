27ab74c8c4210cf383b604da5f335b566be770c0
minh quang <minhquangkid@gmail.com>
2023-07-12 21:00:04 +0700

    fix url admin

--- admin/src/index.js
+++ admin/src/index.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App.jsx';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App.jsx";
+import reportWebVitals from "./reportWebVitals";
+import { disableReactDevTools } from "@fvilers/disable-react-devtools";
 
-ReactDOM.render( <
-	React.StrictMode >
-	<
-	App / >
-	<
-	/React.StrictMode>,
-	document.getElementById('root')
+if (process.env.NODE_ENV === "production") disableReactDevTools();
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
