c70f8abe8229bd5e2835cb2052325bd5b041ce80
minh quang <minhquangkid@gmail.com>
2023-07-16 15:37:53 +0700

    fix url

--- client/src/API/axiosClient.jsx
+++ client/src/API/axiosClient.jsx
@@ -5,8 +5,7 @@ import queryString from "query-string";
 // Please have a look at here `https://github.com/axios/axios#requestconfig` for the full list of configs
 const axiosClient = axios.create({
   // baseURL: "http://localhost:5000",
-  // baseURL: "https://electronic-api.onrender.com",
-  baseURL: "https://electronic-api.quangapi.com",
+  baseURL: "https://electronic-api.onrender.com",
   headers: {
     "content-type": "application/json",
     // 'CSRF-Token':
