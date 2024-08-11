4399f24c5e75aad87f6c2c7da6bb3c330d61a504
quang <minhquangkid@gmail.com>
2024-08-11 17:40:46 +0700

    styling

--- client/src/History/Component/DetailHistory.jsx
+++ client/src/History/Component/DetailHistory.jsx
@@ -101,7 +101,7 @@ function DetailHistory(props) {
                     <h6 className="mb-0">{value.productId.name}</h6>
                   </td>
                   <td className="align-middle border-0">
-                    <h6 className="mb-0">{value.productId.price}</h6>
+                    <h6 className="mb-0">{value.productId.price} (VNĐ)</h6>
                   </td>
                   <td className="align-middle border-0">
                     <h6 className="mb-0">{value.quantity}</h6>
